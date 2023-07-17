24685ae9ff8e24ae6acf4bd343ef3018276add13
eswar7981 <eswarsatyavarapu7981@gmail.com>
2023-07-17 18:34:04 +0530

    selectquery

--- index1.js
+++ index1.js
@@ -5,3 +5,13 @@ a[2].style.backgroundColor='green'
 for(var i=0;i<4;i++){
     a[i].style.fontWeight='bold'
 }
+
+var a=document.getElementsByClassName('list-group-item')
+
+a[4].style.backgroundColor='red'
+ 
+var b=document.getElementById('items')
+var c=document.querySelector('.list-group-item:nth-child(2)')
+c.style.backgroundColor='green'
+var d=document.getElementsByClassName('list-group-item')
+d[1].remove()
