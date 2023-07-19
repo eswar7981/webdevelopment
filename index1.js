76cb24d7ad0897085059ae115bcd39d75f0d1444
eswar7981 <eswarsatyavarapu7981@gmail.com>
2023-07-19 10:18:01 +0530

    stored as object

--- index1.js
+++ index1.js
@@ -7,8 +7,11 @@ function addItem(e){
     e.preventDefault();
     var newitem=document.getElementById('item').value;
     var newitem1=document.getElementById('item1').value;
-    localStorage.setItem('name',newitem)
-    localStorage.setItem('description',newitem1)
+    myobj={name:newitem,description:newitem1}
+    let obj=JSON.stringify(myobj);
+    localStorage.setItem('myobj',obj)
+
+
     var newli=document.createElement('li');
     newli.className='list-group-item';
     var temp=newitem+' '+newitem1
