69a7f750fd1b19f35363757072af866903512214
eswar7981 <eswarsatyavarapu7981@gmail.com>
2023-07-20 11:35:55 +0530

    added edit option

--- booking.js
+++ booking.js
@@ -19,17 +19,37 @@ function addItem(e){
 
     myobj={name:name,email:email,phonenumber:phonenumber}
     let obj=JSON.stringify(myobj);
-    localStorage.setItem('obj',obj)
+    localStorage.setItem(name,obj)
 
     
     const delbutton=document.createElement('input');
     delbutton.type='button'
     delbutton.value="Delete"
     delbutton.onclick=() =>{
-        localStorage.removeItem('obj')
+        localStorage.removeItem(name)
         itemList.removeChild(newli)
     }
+     
     newli.appendChild(delbutton);
+    const edit=document.createElement('input');
+    edit.type='button'
+    edit.value="Edit"
+    edit.onclick=() =>{
+      
+        var s=document.getElementById("form")
+        var abc=localStorage.getItem(name)
+        console.log(abc)
+        abc=JSON.parse(abc)
+        s[0].value=abc['name']
+        s[1].value=abc["phonenumber"]
+        s[2].value=abc["email"]
+       
+
+
+        localStorage.removeItem(name)
+        itemList.removeChild(newli)
+    }
+    newli.appendChild(edit);
     
    
 
