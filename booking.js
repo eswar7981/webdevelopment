eab2861545e6df75f0c96b0f0018eca32a0851fd
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-08-01 10:52:25 +0530

    Update booking.js

--- booking.js
+++ booking.js
@@ -18,7 +18,10 @@ function addItem(e){
     
 
     myobj={name:name,email:email,phonenumber:phonenumber}
+<<<<<<< HEAD
+=======
 
+>>>>>>> a0a7322ee437e3fc704f4c4e9973c623abecf173
     obj={
         name,email,phonenumber
     }
@@ -28,7 +31,10 @@ function addItem(e){
     console.log(res))
     .catch((err)=>
     console.log(err))
+<<<<<<< HEAD
+=======
 
+>>>>>>> a0a7322ee437e3fc704f4c4e9973c623abecf173
 
     
     const delbutton=document.createElement('input');
@@ -65,4 +71,52 @@ function addItem(e){
 
   
 
+}
+
+
+window.addEventListener("DOMContentLoaded",() =>{
+    axios.get("https://crudcrud.com/api/963301cc8a974ef89266b0d96b77fb82/appointmentdata")
+    .then((response)=>{
+    console.log(response)
+    for(var i=0;i<response.data.length;i++){
+        console.log(showNewUser(response.data[i]))
+    }
+})
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
+function showNewUser(user){
+    var newli=document.createElement('li');
+    newli.className="list-group";
+    var temp=user.name+' '+user.phonenumber+" "+user.email
+    newli.appendChild(document.createTextNode(temp));
+    itemList.appendChild(newli)
+    const delbutton=document.createElement('input');
+    delbutton.type='button'
+    delbutton.value="Delete"
+    delbutton.onclick=() =>{
+        localStorage.removeItem(name)
+        itemList.removeChild(newli)
+    }
+     
+    newli.appendChild(delbutton);
+    const edit=document.createElement('input');
+    edit.type='button'
+    edit.value="Edit"
+    edit.onclick=() =>{
+        var s=document.getElementById("form")
+        s[0].value=user.name
+        s[1].value=user.phonenumber
+        s[2].value=user.email
+       
+
+
+        localStorage.removeItem(user.name)
+        itemList.removeChild(newli)
+    }
+    newli.appendChild(edit);
+    
+   
 }
