1b5c258b14008fa39e1bdf6ea096da1f476e9acf
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-08-11 21:22:52 +0530

    Update and rename expense to expense.js

--- expense.js
+++ expense.js
@@ -1,19 +1,24 @@
 import './expenseItem.css';
 
 
-function ExpenseItem(){
+function ExpenseItem(loca){
+
+
+
     return (
         <div className="expense-item">
             <div>Date</div>
             <div className="expense-item__description">
                 <h2>Expenses Items</h2>
-                <a>Food Rs 10</a><br></br>
+                <a>Food Rs </a><br></br>
                 <a>Petrol Rs 100</a><br></br>
                 <div className='expense-item__price'><a>Movies Rs 200</a><br></br>
                 </div>
+               
                 <a>locationofExpenditure</a>
                 <a>Shopping mall</a>
-
+                <a>{loca.locat}</a>
+             
             </div>
         </div>
       )
