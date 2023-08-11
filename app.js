c27f3bb5525bf2f3bc343b2ee1fc0fdc699afae5
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-08-11 21:21:52 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+import ExpenseItem from './expenseitem';
+function App() {
+  const location=[{id:"Home"}];
+return(
+<ExpenseItem locat={location[0].id}> </ExpenseItem>);
+      
+     
+      
+}
+
+export default App;
