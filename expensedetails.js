87e2775113eb2e23abdae76929a8d8cb2e90ecc3
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-08-18 09:38:10 +0530

    Create expensedetails.js

---
+++ expensedetails.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './expenseitem';
+
+
+function Expensedetails() {
+    const amount="1000"
+    const location="Home"
+    const date="11-01-2021"
+    const title="expenses"
+return(
+<ExpenseItem  amount1={amount} location1={location} date1={date} title1={title}/>
+
+)}
+
+export default Expensedetails;
