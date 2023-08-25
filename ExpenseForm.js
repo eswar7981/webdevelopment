fe3983da366a38c225c3b589a6c1f1b590038421
eswar7981 <126505498+eswar7981@users.noreply.github.com>
2023-08-25 19:03:07 +0530

    added useState in  ExpenseForm.js

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -1,34 +1,50 @@
-import './ExpenseForm.css'
+const ExpenseForm = () => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
+  const func1= (e) => {
+    setTitle(e.target.value);
+  };
+   
+
+  const func2= (e) => {
+    setDate(e.target.value);
+  };
+
+  const func3= (e) => {
+    setAmount(e.target.value);
+  };
 
 
-import {useState} from 'react' 
 
-const ExpenseForm=()=>{
-       const [title,setTitle]=useState("nothing")
-       const [amount,setAmount]=useState("nothing")
-       const [date,setDate]=useState("nothing")
-      
-const func=(e)=>{
-    setTitle(e.target.value)
-    
-}
-
-
-return(
+  return (
     <div className="new-expense">
-        <form>
-            <label>Title</label><br></br>
-            <div classNxame="expenses">
-            <label>{title}</label>
-            </div>
-            <br></br>
-            <input onChange={func}></input>
-        </form>
-       <button >submit</button>
-
+      <form>
+        <label>Title</label>
+        <br></br>
+        <input type="text" onChange={func1}></input>
+        <br></br>
+        <div className="value-holder">
+        <label>{title}</label>
+        </div>
+        <br></br>
+        <label>Date</label>
+        <input type="date" min="2011-08-2" onChange={func2}></input>
+        <br></br>
+        <label>{date}</label>
+        <br></br>
+        <label>Amount</label>
+        <input type="number"  onChange={func3}></input>
+        <br></br>
+        <label>{amount}</label>
+        <br></br>
+        
+      </form>
+      <button>submit</button>
     </div>
-)
-}
+  );
+};
 
-
-export default ExpenseForm
+export default ExpenseForm;
+ry
